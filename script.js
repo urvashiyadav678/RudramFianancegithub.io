8be572f61c3a3f781d4ca3e051e370dcfa3232a0
Urvashi yadav <139036141+urvashiyadav678@users.noreply.github.com>
2023-08-14 20:12:44 +0530

    Update script.js
    add nav-js

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ tl.from(".title h1, .title h3",{
     stagger:0.3
 
 })
+
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navMenu");
+
+hamburger.addEventListener("click",()=>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-links").forEach(n=> n.addEventListener("click", () =>{
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+
+}))
